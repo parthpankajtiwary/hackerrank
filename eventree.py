63e95089652259d07c2ac04231ecfe64b9dc01cd
parthpankajtiwary <parthpankajtiwary@gmail.com>
2014-11-23 20:41:01 +0530

    Even tree using list

--- eventree.py
+++ eventree.py
@@ -1,42 +1,34 @@
-
-
-def search(a,b):
-    seen = {}
-    seen[a] = True
-    q = []
-
-    for x in adj[a]:
-        if x != b:
-            seen[x] = True
-            q.append(x)
-    s = len(q) + 1
-    while q:
-        n = q.pop()
-        for x in adj[n]:
-            if x not in seen:
-                seen[x] = True
-                q.append(x)
-                s += 1
-    return (s+1)%2
-
-[N,M] = [int(x) for x in raw_input().split(' ')]
+N, M = map(int, raw_input().split())
 edges = []
-adj = {}
-for a in xrange(1,N+1):
-    adj[a] = []
+for x in xrange(M):
+    u, v = map(int, raw_input().split())
+    edges.append([u, v])
 
-for a in xrange(M):
-    [x,y] = [int(z) for z in raw_input().split(' ')]
-    adj[x].append(y)
-    adj[y].append(x)
-    edges.append((x,y))
+
+info = []
+
+def findChildren(n):
+    children = []
+    for x in xrange(M):
+        if edges[x][1] == n:
+            children.append(edges[x][0])
+            childN = findChildren(edges[x][0])
+            for child in childN:
+                children.append(child)
+    return children
+tree = []
+def generateTree():
+    global tree
+    for x in xrange(N):
+		tree.append([x+1])
+    for x in xrange(N):
+		tree[x].append(findChildren(x+1))
+    return tree
+
+generateTree()
 
 count = 0
-for e in edges:
-    (x,y) = e
-    if search(x,y):
+for x in xrange(N):
+    if len(tree[x][1]) % 2 == 1:
         count += 1
-        adj[x].remove(y)
-        adj[y].remove(x)
-
-print count
+print count - 1
